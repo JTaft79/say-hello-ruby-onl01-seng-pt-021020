0199896f5691e54edeae1c1fbde4e162a3913f70
Julene Taft <coachjulene@gmail.com>
2020-02-13 21:02:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
+say_hello
 # Build your say_hello method here
+say_hello ("Gabriela")
+# I call on the method, say_hello, and give it the string "Gabriela" 
+say_hello("Gabriela")
+# say_hello "Gabriela"
+say_hello("Gabriela")
+puts say_hello "Gabriela"
+print say_hello "Gabriela"
+# say_hello "Gabriela"
+"Hello Ruby Programmer!"
